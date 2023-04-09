c076e767a317e5d94118e6ad7743353cd7f28d15
deepankarvarma <satwikdpshrit@gmail.com>
2023-04-09 16:52:51 +0530

    minor

--- app.py
+++ app.py
@@ -30,7 +30,8 @@ st.markdown(f"<h3>Visualization for {ticker_input}</h3>",unsafe_allow_html=True)
 stock_data = get_stock_data(ticker_input)
 
 # Display the stock data in a line chart
-st.line_chart(stock_data['Close'])
+chart_data = {'data': stock_data['Close'], 'name': 'Closing Price'}
+st.line_chart(chart_data, use_container_width=True)
 
 # Display the stock data in a table
 st.write(stock_data)
